cd249ed50563ffc166eca9886e76f0178e1e30f8
Fábio Ferreira <ffcfpten@gmail.com>
2019-10-17 19:10:44 +0100

    feat: forgot to commit binary representation

--- imperative-programming/sheet4/binary_representation.c
+++ imperative-programming/sheet4/binary_representation.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
+int binary(int n) {
+  int v = 0, tens = 1;
+
+  int quocient = n;
+  while (quocient) {
+    v += (quocient % 2) * tens;
+
+    quocient /= 2;
+    tens *= 10;
+  }
+
+  return v;
+}
+
 int main() {
   int n;
   
   scanf("%d", &n);
 
-  int quocient = n;
-  while (quocient) {
-    printf("%d", quocient % 2);
-
-    quocient /= 2;
-  }
-
-  printf("\n");
+  printf("%d\n", binary(n));
   return 0;
 }
