315fb96c56703d0d170326648efcd51519bb86e0
Fábio Ferreira <ffcfpten@gmail.com>
2019-10-21 14:58:51 +0100

    feat: print primes from 1 to n

---
+++ imperative-programming/sheet5/prime.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void printPrime(long n) {
+  if (n <= 1) return;
+
+  for (long i = 2; i < n; i++) {
+    if (i == 2) printf("2 ");
+
+    if (i % 2 != 0) {
+      printf("%ld ", i);
+    }
+  }
+}
+
+int main() {
+  long int n;
+  scanf("%ld", &n);
+
+  printPrime(n);
+
+  printf("\n");
+}
