9cb099ef858fd415e97a431ff373ae843fd9f274
Fábio Ferreira <ffcfpten@gmail.com>
2020-01-17 23:17:14 +0000

    exam exercise: sum of divisors of n

---
+++ imperative-programming/exam/A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int soma_div(int n) {
+  int sum = 0;
+
+  for (int i = 1; i < n; i++) if (n % i == 0) sum += i;
+
+  return sum;
+}
+
+int main() {
+  int a = 10;
+
+  printf("%d\n", soma_div(a));
+
+  return 0;
+}
