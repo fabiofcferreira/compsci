6b705b2915e17b28ae477fddfa402f186c30a004
Fábio Ferreira <ffcfpten@gmail.com>
2020-02-21 00:40:30 +0000

    feat: tree symbol

---
+++ Y1/S2/cc1007/sheet1/Tree.java
@@ -0,0 +1,28 @@
+public class Tree {
+  static void line(int spaces, int hashtags) {
+    // print spaces before hashtags
+    for (int i = 0; i < spaces; i++) System.out.print(' ');
+    // print hashtags
+    for (int i = 0; i < hashtags; i++) System.out.print('#');
+    // print spaces after hashtags
+    for (int i = 0; i < spaces; i++) System.out.print(' ');
+
+    System.out.println();
+  }
+
+  static void tree(int maxHashtag) {
+    int lineSize = 9;
+
+    for(int i = 1; i <= maxHashtag; i += 2) {
+      line((lineSize - i) / 2, i);
+    }
+
+    for(int i = maxHashtag-2; i >= 1; i -= 2) {
+      line((lineSize - i) / 2, i);
+    }
+  }
+
+  public static void main(String[] args) {
+    tree(5);
+  }
+}
