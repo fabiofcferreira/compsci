b2b5fb6cb7e9b73075f293e73ab0b397f1dfe600
Fábio Ferreira <ffcfpten@gmail.com>
2020-02-21 00:15:05 +0000

    feat: functional programming class folder added and few exercises from class 1

---
+++ Y1/S2/cc1005/Exercises/math.hs
@@ -0,0 +1,24 @@
+double x = x + x
+quadruple x = double (double x)
+factorial n = product [1..n]
+average x y = (x+y)/2
+
+-- Zero function
+-- zero x = case x of
+--           0 -> True
+--           otherwise -> False
+
+min3 :: Int -> Int -> Int -> Int
+min3 a b c = minimum [a, b, c]
+
+eq3 :: Int -> Int -> Int -> Bool
+eq3 a b c = if (a == b && b == c) then True else False
+
+avg :: Float -> Float -> Float
+avg a b = (a+b)/2
+
+evenNumber :: Int -> Bool
+evenNumber a = if (mod a 2) == 0 then True else False
+
+sign :: Int -> Int
+sign a = if (a < 0) then -1 else 1
