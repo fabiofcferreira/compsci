7c6f781a2ddc4b253f09bdc14b38a6e7c9f2014e
Fábio Ferreira <ffcfpten@gmail.com>
2020-03-05 12:44:12 +0000

    feat: ED183

---
+++ Y1/S2/cc1007/sheets/sheet2/ed183.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ed183 {
+  public static void main(String[] args) {
+    int nOperands = 0;
+    float sum = 0;
+    int min = 0;
+    int max = 0;
+
+    Scanner sc = new Scanner(System.in);
+
+    // read number of operands
+    nOperands = sc.nextInt();
+
+    // read operands values
+    for (int i = 0; i < nOperands; i++) {
+      int a = sc.nextInt();
+
+      if (a < min) {
+        min = a;
+      }
+      if (a > max) {
+        max = a;
+      }
+
+      sum += a;
+    }
+
+    System.out.printf("%.2f\n", sum / nOperands);
+    System.out.println(Math.abs(max - min));
+
+
+    sc.close();
+  }
+}
