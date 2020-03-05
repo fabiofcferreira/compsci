de790af71f73dcc8d52312b360ea64a48dbd7f97
Fábio Ferreira <ffcfpten@gmail.com>
2020-03-05 11:01:06 +0000

    feat: quizz 2

---
+++ Y1/S2/cc1007/quizzes/quiz2/Point.java
@@ -0,0 +1,18 @@
+// Uma classe simples para representar um ponto 2D
+class Point {
+   int x, y;
+
+   Point() {
+      x = y = 0;
+   }
+   
+   Point(int x0, int y0) {
+      x = x0;
+      y = y0;
+   }
+
+   // Devolve uma representação em texto do conteúdo de um Ponto
+   public String toString() {
+      return "(" + x + "," + y + ")";
+   }
+}
