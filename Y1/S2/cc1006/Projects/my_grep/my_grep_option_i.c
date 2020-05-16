0d5237bf1677dcbfcd86420916ce4690abf03d7b
Fábio Ferreira <ffcfpten@gmail.com>
2020-05-16 22:50:58 +0100

    cc1006: my_grep -i and -l options

--- Y1/S2/cc1006/Projects/my_grep/my_grep_option_i.c
+++ Y1/S2/cc1006/Projects/my_grep/my_grep_option_i.c
@@ -4,12 +4,18 @@
 #include <string.h>
 #include <unistd.h>
 
-int OP_l = 0;
+int OP_i = 0;
 
 void searchFile(char* word, char* filePath) {
   FILE* f;
   char* line;
   size_t len = 0;
+
+  char* lowerCaseLine;
+  char* lowerCaseWord = (char *) malloc(strlen(word) * sizeof(char)) ;
+
+  // convert word to lowercase
+  for (int i = 0; word[i] != '\0'; i++) lowerCaseWord[i] = tolower(word[i]);
 
   f = fopen(filePath, "r");
   if (f == NULL) {
@@ -18,14 +24,18 @@ void searchFile(char* word, char* filePath) {
   }
 
   while ( getline(&line, &len, f) != -1) {
+    lowerCaseLine = (char *) malloc(strlen(line) * sizeof(char));
+    // convert line to lowercase
+    for (int i = 0; line[i] != '\0'; i++) lowerCaseLine[i] = tolower(line[i]);
+
     // normal use without -l flag
-    if (!OP_l && strstr(line, word) != NULL) {
+    if (!OP_i && strstr(line, word) != NULL) {
       printf("%s", line);
     }
 
     // only print file name which has a line with a match
-    if (OP_l && strstr(line, word) != NULL) {
-      printf("%s\n", filePath);
+    if (OP_i && strstr(lowerCaseLine, lowerCaseWord) != NULL) {
+      printf("%s", line);
       break;
     }
   }
@@ -43,8 +53,8 @@ int main(int argc, char** argv) {
   // parse arguments
   while ((opt = getopt(argc, argv, ":if:l")) != -1) {
     switch (opt) {
-      case 'l':
-        OP_l = 1;
+      case 'i':
+        OP_i = 1;
         break;
     }
   }
