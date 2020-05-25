30d2efa9c4fe589d7e50e16b60c1e6baf8a4d221
Fábio Ferreira <ffcfpten@gmail.com>
2020-05-25 15:55:58 +0100

    cc1007: ed195

--- Y1/S2/cc1007/Exercises/Stack/ED195.java
+++ Y1/S2/cc1007/Exercises/Stack/ED195.java
@@ -1,22 +1,4 @@
 public class ED195 {
-  public static void reverse(MyStack<Character> s, int n) {
-    // if the number of reversed elements is 1
-    // there's nothing to do
-    if (s.size() == 1) return;
-
-    char[] elements = new char[n];
-
-    // remove the first n elements of the stack
-    for (int i = 0; i < n; i++) {
-      elements[i] = s.pop();
-    }
-
-    // re-add the elements in reverse order
-    for (int i = 0; i < n; i++) {
-      s.push(elements[i]);
-    }
-  }
-
   public static boolean balanced(String s) {
     // in order for the expression to be valid
     // it's length must be even (because each symbol
