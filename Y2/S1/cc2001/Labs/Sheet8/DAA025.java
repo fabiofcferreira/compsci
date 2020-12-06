5a716f924a563498708996b5413ff9e64b615a1d
Fábio Ferreira <ffcfpten@gmail.com>
2020-12-06 18:17:12 +0000

    cc2002: DAA025

---
+++ Y2/S1/cc2001/Labs/Sheet8/DAA025.java
@@ -0,0 +1,47 @@
+public class DAA025 {
+  static int n;              // Nodes number
+  static boolean adj[][];    // Adjacent matrix
+  static boolean visited[];  // Visited matrix
+
+  static int circuitsCounter = 0;
+
+  static void dfs(int v) {
+    visited[v] = true;
+    for (int i = 1; i <= n; i++) {
+      if (adj[v][i] && !visited[i]) {
+        dfs(i);
+      }
+    }
+  }
+    
+  public static void main(String args[]) {
+    FastScanner stdin = new FastScanner(System.in);
+    
+    // Read number of nodes
+    n = stdin.nextInt();
+    
+    // Create adjacent and visited arrays
+	  adj = new boolean[n+1][n+1];
+    visited = new boolean[n+1];
+    
+    // Read number of edges and read edges
+	  int edges = stdin.nextInt();	
+	  for (int i = 0; i < edges; i++) {
+	    int a = stdin.nextInt();
+      int b = stdin.nextInt();
+
+	    adj[a][b] = adj[b][a] = true;
+    }
+    
+    // Start a DFS on all unvisited nodes
+    for (int i = 1; i < adj.length; i++) {
+      if (!visited[i]) {
+        circuitsCounter++;
+        dfs(i);
+      }
+    }
+
+    FastPrint.out.println(circuitsCounter);
+    FastPrint.out.close();
+  }
+}
